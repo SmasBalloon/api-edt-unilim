4fb0f8fc4dcfec4dfc23bb05205fc3ad0ced1e62
SmasBalloon <mathissauvage24000@gmail.com>
2025-11-02 15:36:34 +0100

    feat: ajouter les fichiers de base pour le contrôleur, le service et le module de l'application

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { EdtModule } from './edt/edt.module.js';
+import { PdfDownloaderModule } from './pdf-downloader/pdf-downloader.module.js';
+import { PdfDownloaderController } from './pdf-downloader/pdf-downloader.controller.js';
+import { PdfDownloaderService } from './pdf-downloader/pdf-downloader.service.js';
+
+@Module({
+  imports: [EdtModule, PdfDownloaderModule],
+  controllers: [AppController, PdfDownloaderController],
+  providers: [AppService, PdfDownloaderService],
+})
+export class AppModule { }
