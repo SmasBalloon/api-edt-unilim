f24b027e097cff94fd08e07d5e261988ccb1e671
Mathis Sauvage <165672108+SmasBalloon@users.noreply.github.com>
2025-11-06 18:58:02 +0100

    feat: update A1, A2, and A3 endpoint methods to be asynchronous

--- src/pdf-watcher/pdf-watcher.controller.ts
+++ src/pdf-watcher/pdf-watcher.controller.ts
@@ -6,17 +6,17 @@ export class PdfWatcherController {
   constructor(private readonly pdfWatcherService: PdfWatcherService) {}
 
   @Get('A1')
-  getPagesA1(): void {
-    this.pdfWatcherService.getPagesA1();
+  async getPagesA1(): Promise<void> {
+    await this.pdfWatcherService.getPagesA1();
   }
 
   @Get('A2')
-  getPagesA2(): void {
-    this.pdfWatcherService.getPagesA2();
+  async getPagesA2(): Promise<void> {
+    await this.pdfWatcherService.getPagesA2();
   }
 
   @Get('A3')
-  getPagesA3(): void {
-    this.pdfWatcherService.getPagesA3();
+  async getPagesA3(): Promise<void> {
+    await this.pdfWatcherService.getPagesA3();
   }
 }
