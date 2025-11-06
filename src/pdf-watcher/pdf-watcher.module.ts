2fcbac06cc801a8cd416a900ef648c62a0fb5a03
Mathis Sauvage <165672108+SmasBalloon@users.noreply.github.com>
2025-11-06 16:39:04 +0100

    feat: update PdfWatcherModule to include PdfWatcherService

--- src/pdf-watcher/pdf-watcher.module.ts
+++ src/pdf-watcher/pdf-watcher.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PdfWatcherController } from './pdf-watcher.controller';
+import { PdfWatcherController } from './pdf-watcher.controller.js';
+import { PdfWatcherService } from './pdf-watcher.service.js';
 
 @Module({
-  controllers: [PdfWatcherController]
+  controllers: [PdfWatcherController],
+  providers: [PdfWatcherService],
 })
 export class PdfWatcherModule {}
