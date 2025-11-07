2ced63859380b5168543f656bec1b991c299e7ac
Mathis Sauvage <165672108+SmasBalloon@users.noreply.github.com>
2025-11-07 09:50:28 +0100

    feat: update PdfDownloaderController to handle PDF downloads conditionally

--- src/pdf-downloader/pdf-downloader.controller.ts
+++ src/pdf-downloader/pdf-downloader.controller.ts
@@ -1,23 +1,18 @@
 import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { PdfDownloaderService } from './pdf-downloader.service.js';
+import fs from 'fs';
 
 @Controller('pdf-downloader')
 export class PdfDownloaderController {
   constructor(private readonly pdfDownloaderService: PdfDownloaderService) {}
 
-  @Get(':annee/:groupe')
-  async DownloadPdf(
-    @Param('annee') annee: number,
-    @Param('groupe') groupe: number,
-  ) {
-    const pdfUrl = `https://edt-iut-info.unilim.fr/edt/A${annee}/A${annee}_S9.pdf`;
-    const dest = `./pdf/A${annee}_S9.pdf`;
-    this.pdfDownloaderService.downloadPdf(pdfUrl, dest);
-  }
-
-  @Delete(':annee')
-  async deletePdf(@Param('annee') annee: number) {
-    const dest = `./pdf/A${annee}_S9.pdf`;
-    await this.pdfDownloaderService.deletePdf(dest);
+  @Get(':annee')
+  DownloadPdf(@Param('annee') annee: string) {
+    const years = annee.replace('.pdf', ' ').split('_')[0];
+    const pdfUrl = `https://edt-iut-info.unilim.fr/edt/${years}/${annee}`;
+    const dest = `./pdf/${years}/${annee}`;
+    if (!fs.existsSync(dest)) {
+      this.pdfDownloaderService.downloadPdf(pdfUrl, dest);
+    }
   }
 }
