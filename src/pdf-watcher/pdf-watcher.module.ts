147de2dd4d8aec6ff0ae969a3da51d175cea6635
Mathis Sauvage <165672108+SmasBalloon@users.noreply.github.com>
2025-11-07 09:50:51 +0100

    feat: add ScheduleModule to PdfWatcherModule for scheduling tasks

--- src/pdf-watcher/pdf-watcher.module.ts
+++ src/pdf-watcher/pdf-watcher.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { PdfWatcherController } from './pdf-watcher.controller.js';
 import { PdfWatcherService } from './pdf-watcher.service.js';
 
 @Module({
   controllers: [PdfWatcherController],
   providers: [PdfWatcherService],
+  imports: [ScheduleModule.forRoot()],
 })
 export class PdfWatcherModule {}
